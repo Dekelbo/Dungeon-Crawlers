505b90e895817d4c3c106cd3a2fff1abb45d1708
Tomer Gal <tgal2@apple.com>
2025-01-25 16:17:39 +0200

    implement attack() and attacked() in Sorcerer

--- Sorcerer.cpp
+++ Sorcerer.cpp
@@ -4,9 +4,19 @@ Sorcerer::Sorcerer(int max_life, int damage) : Player("Sorcerer", max_life, dama
 Sorcerer::~Sorcerer() {
 }
 
-int special_power() {
-
+int Sorcerer::attacked() {
+	*this -= *(this->fighting_who);
+	return this->fighting_who->getDamage();
 }
-void Sorcerer::attack() {
 
+int Sorcerer::attack() {
+    this->special_attack_wait = (this->special_attack_wait + 1) % WAITING_TIME; // update the turns - range 0-4
+    if (special_attack_wait == 1) {
+		this->damage *= 2; // special power is on, double demage
+	}
+	int demage_dealt = this->fighting_who->attacked();
+	if (special_attack_wait == 1) {
+		this->damage /= 2; // already used special power, back to normal demage
+	}
+	return demage_dealt;
 }
